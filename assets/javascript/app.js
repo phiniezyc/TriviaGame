cfef0f8bbbfefab035d746c67621c2b34008819e
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-06 02:58:52 -0400

    started over w/ new HTML page-beginning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,36 @@
-console.log("Test");
+
+//Global Variables ==================================================
+
+
+
+
+
+
+//Functions ===========================================================
+/*function startQuiz() {
+    document.getElementById("startQuiz").addEventListener("click", Quiz1)
+
+}
+
+function Quiz1() {
+    document.getElementById("quizQuestion").innerHTML = "Who was Roger Rabbit Sleeping w/?";
+    document.getElementById("question1Div").innerHTML = "Jessica Rabbit";
+    //named "startQuiz because it's the middle section for the button
+    document.getElementById("startQuiz").innerHTML = "Elmer Fudd";
+    document.getElementById("question3Div").innerHTML = "Roger Himself";
+    document.getElementById("question4Div").innerHTML = "You did!";
+    console.log("test")
+}*/
+
+$("#startGameButton").on("click", function() {
+    $("#startGameButton").remove();
+});
+
+
+
+// Process =============================================================
+
+
+
+//startQuiz();
+
