c41430cfc65df8f7248f406618cc067397cf307e
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-07 06:01:13 -0400

    questions added to page: no timer or other functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,3 @@
-
-//Global Variables ==================================================
-
-
-
-
-
-
-//Functions ===========================================================
 /*function startQuiz() {
     document.getElementById("startQuiz").addEventListener("click", Quiz1)
 
@@ -22,15 +13,31 @@ function Quiz1() {
     console.log("test")
 }*/
 
-$("#startGameButton").on("click", function() {
+$("#startGameButton").on("click", function () {
     $("#startGameButton").remove();
+    for (var i = 0; i < questions.length; i++) {
+        $("#questionSection").append("<h2>" + questions[i].question + "</h2>");
+        for (var j = 0; j < questions[i].answers.length; j++) {
+            $("#questionSection").append("<input type = 'radio' name= 'question- " + i + " 'value'" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+        }
+    }
 });
 
-
-
-// Process =============================================================
-
-
-
-//startQuiz();
+var questions = [{
+    question: "who shot Roger Rabbit?",
+    answers: ["Jessica Rabbit", "The Tortoise", "Elmer Fudd", "Roger Rabbit?"],
+    correctAnswer: "Jessica Rabbit"
+}, {
+    question: "Who is the best Brave?",
+    answers: ["Freeman", "Inciarte", "Tehran", "Swanson"],
+    correctAnswer: "Freeman"
+}, {
+    question: "Who is the best Hawk of All Time?",
+    answers: ["Dominique Wilkins", "Joe Johnson", "Pistol Pete", "Dikembe Mutombo"],
+    correctAnswer: "Dominique Wilkins"
+}, {
+    question: "Who is the best Falcon?",
+    answers: ["Julio Jones", "Matt Ryan", "Devante Freeman", "Beasley"],
+    correctAnswer: "Julio Jones"
+}];
 
