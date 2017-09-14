bedd69f803dea6a3a1a42b4694fd9b02604e901f
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-14 02:38:31 -0400

    added timer countdown to working radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,96 @@
-/*function startQuiz() {
-    document.getElementById("startQuiz").addEventListener("click", Quiz1)
-
-}
-
-function Quiz1() {
-    document.getElementById("quizQuestion").innerHTML = "Who was Roger Rabbit Sleeping w/?";
-    document.getElementById("question1Div").innerHTML = "Jessica Rabbit";
-    //named "startQuiz because it's the middle section for the button
-    document.getElementById("startQuiz").innerHTML = "Elmer Fudd";
-    document.getElementById("question3Div").innerHTML = "Roger Himself";
-    document.getElementById("question4Div").innerHTML = "You did!";
-    console.log("test")
-}*/
 
 $("#startGameButton").on("click", function () {
-    $("#startGameButton").remove();
+    trivaScoring.startTimer();
+    /* $("#startGameButton").remove();
     for (var i = 0; i < questions.length; i++) {
         $("#questionSection").append("<h2>" + questions[i].question + "</h2>");
         for (var j = 0; j < questions[i].answers.length; j++) {
-            $("#questionSection").append("<input type = 'radio' name= 'question- " + i + " 'value'" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            $("#questionSection").append("<input type = 'radio' name= 'questionsToAsk" + i + " 'value' " + questions[i].answers[j] + "'>" + questions[i].answers[j])
         }
     }
+    $("#questionSection").after("<button value = 'hello' id = 'doneButton'> Done"); */
 });
 
 var questions = [{
-    question: "who shot Roger Rabbit?",
-    answers: ["Jessica Rabbit", "The Tortoise", "Elmer Fudd", "Roger Rabbit?"],
-    correctAnswer: "Jessica Rabbit"
+    question: " who shot Roger Rabbit? ",
+    answers: [" Jessica Rabbit ", " The Tortoise ", " Elmer Fudd ", " Roger Rabbit? "],
+    correctAnswer: " Jessica Rabbit "
 }, {
-    question: "Who is the best Brave?",
-    answers: ["Freeman", "Inciarte", "Tehran", "Swanson"],
+    question: " Who is the best Brave? ",
+    answers: [" Freeman ", " Inciarte ", " Tehran ", " Swanson "],
     correctAnswer: "Freeman"
 }, {
-    question: "Who is the best Hawk of All Time?",
-    answers: ["Dominique Wilkins", "Joe Johnson", "Pistol Pete", "Dikembe Mutombo"],
-    correctAnswer: "Dominique Wilkins"
+    question: " Who is the best Hawk of All Time? ",
+    answers: [" Dominique Wilkins ", " Joe Johnson ", " Pistol Pete ", " Dikembe Mutombo "],
+    correctAnswer: " Dominique Wilkins "
 }, {
-    question: "Who is the best Falcon?",
-    answers: ["Julio Jones", "Matt Ryan", "Devante Freeman", "Beasley"],
-    correctAnswer: "Julio Jones"
+    question: " Who is the best Falcon? ",
+    answers: [" Julio Jones ", " Matt Ryan ", " Devante Freeman ", " Beasley "],
+    correctAnswer: " Julio Jones "
 }];
 
+
+var trivaScoring = {
+    correct: 0,
+    incorrect: 0,
+    counter: 10,
+    timeLeft: function() {
+        trivaScoring.counter--;
+        $("#timeClock").html(trivaScoring.counter);
+        console.log(trivaScoring.counter);
+        if (trivaScoring.counter === 0) {
+            console.log("You're done!");
+            trivaScoring.over();
+        }
+    },
+    startTimer: function() {
+        timer = setInterval(trivaScoring.timeLeft, 1000);
+        $("#questionSection").prepend("<h3>Time Left to Answer: <span id = 'timeClock'>  </span> Seconds </h3>");
+        $("#startGameButton").remove();
+        for (var i = 0; i < questions.length; i++) {
+            $("#questionSection").append("<h2>" + questions[i].question + "</h2>");
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $("#questionSection").append("<input type = 'radio' name= 'questionsToAsk" + i + " 'value' " + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
+        }
+        $("#questionSection").after("<button value = 'hello' id = 'doneButton'> Done");
+    },
+
+    over: function(){
+        $.each($("input[name = 'question-0]': checked"), function() {
+            if($(this).val()===questions[0].correctAnswer) {
+                game.correct++;
+            }else {
+                game.incorrect++;
+            }
+        })
+        $.each($("input[name = 'question-1]': checked"), function() {
+            if($(this).val()===questions[1].correctAnswer) {
+                game.correct++;
+            }else {
+                game.incorrect++;
+            }
+        })
+        $.each($("input[name = 'question-2]': checked"), function() {
+            if($(this).val()===questions[2].correctAnswer) {
+                game.correct++;
+            }else {
+                game.incorrect++;
+            }
+        })
+        $.each($("input[name = 'question-3]': checked"), function() {
+            if($(this).val()===questions[3].correctAnswer) {
+                game.correct++;
+            }else {
+                game.incorrect++;
+            }
+        })
+
+
+
+
+
+    }
+
+
+}
